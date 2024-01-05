e88014f418781d5778f117f46eb2b615da95837b
Richard Carson <r.carson@calian.com>
2024-01-05 10:44:24 -0500

    fix deserialize

--- src/value.rs
+++ src/value.rs
@@ -117,16 +117,17 @@ impl TryFrom<&str> for ValueType {
     type Error = Error;
     fn try_from(s: &str) -> Result<Self, Error> {
         match s.to_lowercase().as_str() {
-            "bool" => Ok(ValueType::Bool),
-            "fixed" => Ok(ValueType::Fixed),
-            "float" => Ok(ValueType::Float),
-            "currency" => Ok(ValueType::Currency),
-            "int" => Ok(ValueType::Int),
-            "string" => Ok(ValueType::String),
-            "array" => Ok(ValueType::Array),
-            "object" => Ok(ValueType::Object),
-            "numeric" => Ok(ValueType::Numeric),
-            "compound" => Ok(ValueType::Compound),
+            "Bool" => Ok(ValueType::Bool),
+            "Fixed" => Ok(ValueType::Fixed),
+            "Float" => Ok(ValueType::Float),
+            "Currency" => Ok(ValueType::Currency),
+            "Int" => Ok(ValueType::Int),
+            "String" => Ok(ValueType::String),
+            "Array" => Ok(ValueType::Array),
+            "Object" => Ok(ValueType::Object),
+            "Numeric" => Ok(ValueType::Numeric),
+            "Compound" => Ok(ValueType::Compound),
+            "Any" => Ok(ValueType::Any),
             _ => Err(Error::UnrecognizedType(s.to_string())),
         }
     }
